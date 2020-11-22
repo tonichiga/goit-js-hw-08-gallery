ede8c2dbb620207c42ed965889e4a7f59dcb5f7f
Anton Chyhariev <tonichiga@gmail.com>
2020-11-22 22:25:38 +0200

    Завершение обязательной части дз 8

--- js/gallery.js
+++ js/gallery.js
@@ -8,9 +8,10 @@ const galleryRef = document.querySelector('.gallery');
 const modalRef = document.querySelector('div.lightbox');
 const btnCloseRef = document.querySelector('button[data-action="close-lightbox"]');
 const imageModalRef = document.querySelector('.lightbox__image');
+const overlayRef = document.querySelector('.lightbox__overlay');
 
 
-galleryItems.forEach(({ preview, original, description }) => {
+galleryItems.forEach(({ preview, original, description }, index, i) => {
 
     const list = document.createElement('li');
     const imageElement = document.createElement('img');
@@ -18,7 +19,9 @@ galleryItems.forEach(({ preview, original, description }) => {
 
     imageElement.setAttribute('src', preview);
     imageLink.setAttribute('href', original);
-    imageElement.setAttribute('data-source', original)
+    imageElement.setAttribute('data-source', original);
+    imageElement.setAttribute('alt', description);
+    imageElement.setAttribute('data-id', index);
 
     galleryRef.append(list);
     list.append(imageLink);
@@ -28,10 +31,41 @@ galleryItems.forEach(({ preview, original, description }) => {
     imageElement.classList.add('gallery__image');
     imageLink.classList.add('gallery__link');
 
+
     galleryRef.addEventListener('click', event => {
+        if (event.target.nodeName !== "IMG") {
+            return;
+        };
         event.preventDefault();
         modalRef.classList.add('is-open')
         imageModalRef.src = event.target.dataset.source;
+
+    });
+    overlayRef.addEventListener('click', event => {
+        if (event.target.nodeName !== "DIV") {
+            return;
+        };
+        modalRef.classList.remove('is-open');
+        imageModalRef.src = ""; 
+    });
+
+    window.addEventListener('keydown', event => {
+        if (event.code === 'Escape') {
+        modalRef.classList.remove('is-open');
+        imageModalRef.src = "";
+        };
+    });
+
+    window.addEventListener('keydown', event => {
+        if (event.code === 'ArrowRight') {
+            if (galleryItems[index + 1] === galleryItems.length) {    
+                return;
+            } e
+
+                console.log(galleryItems[index + 1].original)
+            
+            // imageModalRef.src = galleryItems[index + 1].original;
+        };
     });
 
     btnCloseRef.addEventListener('click', event => {
